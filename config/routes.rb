0658c940f8ed87e2961861fa5b562e45f44cde1a
kc-hkb <7kei54egc@gmail.com>
2023-05-14 08:23:07 +0900

    プロフィール機能追加

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,9 @@ Rails.application.routes.draw do
 
 	resources :articles do
     resources :comments, only:[:new, :create]
+
+    resource :likes, only:[:create, :destroy]
   end
+
+  resource :profile, only:[:show, :edit, :update]
 end
