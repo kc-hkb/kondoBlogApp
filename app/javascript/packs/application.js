d88f0b60913f9f0645ee340c3d4b382d67866d5c
kc-hkb <7kei54egc@gmail.com>
2023-05-15 19:14:37 +0900

    no message

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -15,3 +15,11 @@ require("channels")
 //
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
+
+import $ from 'jquery'
+
+document.addEventListener('DOMContentLoaded', () => {
+  $('.article_title').on('click', () => {
+    window.alert('CLICKED')
+  })
+})
